35518e4346ce79f997a99fe0026aa5049b8e1c73
edubranquinho <edubranquinho@hotmail.com>
2018-02-19 23:30:40 -0300

    iniciando a conexão com o activemq

--- src/main/java/br/com/branquinho/jms/TesteConsumidor.java
+++ src/main/java/br/com/branquinho/jms/TesteConsumidor.java
@@ -1,7 +1,27 @@
 package br.com.branquinho.jms;
 
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.JMSException;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import java.util.Scanner;
+
 public class TesteConsumidor {
 
+    public static void main(String args[]) throws NamingException, JMSException {
 
+        InitialContext context = new InitialContext();
+
+        ConnectionFactory factory = (ConnectionFactory) context.lookup("ConnectionFactory");
+        Connection connection = factory.createConnection();
+        connection.start();
+
+        new Scanner(System.in).nextLine();
+
+        connection.close();
+        context.close();
+
+    }
 
 }
